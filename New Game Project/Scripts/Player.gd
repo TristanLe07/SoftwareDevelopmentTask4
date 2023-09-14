c1c5092bda7c454466b62539b82379bcf5ac3ee3
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2023-09-14 09:40:09 +0800

    i did things

--- New Game Project/Scripts/Player.gd
+++ New Game Project/Scripts/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
  
 const MOVE_SPEED = 300
- 
+var bullet_speed = 2000
+var bullet = preload("res://Player/bullet.tscn")
+
 onready var raycast = $RayCast2D
  
 func _ready():
@@ -25,9 +27,18 @@ func _physics_process(delta):
 	global_rotation = atan2(look_vec.y, look_vec.x)
  
 	if Input.is_action_just_pressed("shoot"):
+		fire()
 		var coll = raycast.get_collider()
 		if raycast.is_colliding() and coll.has_method("kill"):
 			coll.kill()
- 
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
+		 
+
 func kill():
 	get_tree().reload_current_scene()
